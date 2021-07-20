dc0abaac05034744714e79435e844d48f09adb10
lamboktulus1379 <lamboktulus1379@gmail.com>
2021-07-20 13:32:55 +0700

    Initial Commit

---
+++ hello/hello.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	// Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+	
+	// Get greeting message and print it
+	message, err := greetings.Hello("")
+	if (err != nil) {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned message
+	// to the console
+	fmt.Println(message)
+}
