5fbe0be19ce270b1b32a35b0758f52f6b6c1f08c
lamboktulus1379 <lamboktulus1379@gmail.com>
2021-08-08 08:34:54 +0700

    Added slices

---
+++ slices/go.mod
@@ -0,0 +1,5 @@
+module example.com/slices
+
+go 1.16
+
+require golang.org/x/tour v0.1.0
---
+++ slices/slice.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"golang.org/x/tour/pic"
+)
+
+func Pic(dx, dy int) [][]uint8 {
+	ss := make([][]uint8, dy)
+	for y := 0; y < dx; y++ {
+		s := make([]uint8, dx)
+		for x := 0; x < dx; x++ {
+			s[x] = uint8((x + y) / 2)
+		}
+		ss[y] = s
+	}
+
+	return ss
+}
+
+func main() {
+	pic.Show(Pic)
+}
