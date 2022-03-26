8e3e314be6a15da92449921d45f0c6d8515df0d0
lamboktulus1379 <lamboktulus1379@gmail.com>
2022-03-26 14:57:18 +0700

    Added adapter design pattern

---
+++ adapter/main.go
@@ -0,0 +1,62 @@
+package main
+
+type RoundHole struct {
+	Radius int
+}
+
+func (roundHole RoundHole) GetRadius() int {
+	return roundHole.Radius
+}
+
+func (roundHole RoundHole) IsFits(roundPeg RoundPeg) bool {
+	return roundHole.GetRadius() >= roundPeg.GetRadius()
+}
+
+type RoundPeg struct {
+	Radius int
+}
+
+func (roundPeg RoundPeg) GetRadius() int {
+	return roundPeg.Radius
+}
+
+type SquarePeg struct {
+	Width int
+}
+
+func (squarePeg SquarePeg) GetWidth() int {
+	return squarePeg.Width
+}
+
+type SquarePegAdapter struct {
+	*RoundPeg
+}
+
+// func main() {
+// 	fmt.Println("Adapter Design Pattern")
+
+// 	hole := RoundHole{5}
+// 	rPeg := RoundPeg{5}
+
+// 	fit := hole.IsFits(rPeg)
+// 	fmt.Println(fit)
+
+// 	smallSquarePeg := SquarePeg{5}
+// 	largeSquarePeg := SquarePeg{10}
+
+// 	// hole.IsFits(smallSquarePeg)
+
+// 	smallSquarePegAdapter := SquarePegAdapter{
+// 		RoundPeg: &RoundPeg{
+// 			Radius: smallSquarePeg.GetWidth(),
+// 		},
+// 	}
+// 	largeSqurePegAdapter := SquarePegAdapter{
+// 		RoundPeg: &RoundPeg{
+// 			Radius: largeSquarePeg.GetWidth(),
+// 		},
+// 	}
+
+// 	fmt.Println(hole.IsFits(*smallSquarePegAdapter.RoundPeg))
+// 	fmt.Println(hole.IsFits(*largeSqurePegAdapter.RoundPeg))
+// }
---
+++ adapter/main2.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type computer interface {
+	insertIntoLightningPort()
+}
+
+type mac struct {
+}
+
+func (mac *mac) insertIntoLightningPort() {
+	fmt.Println("Lightning connector is plugged into mac machine.")
+}
+
+type windows struct {
+}
+
+func (windows *windows) insertIntoUSBPort() {
+	fmt.Println("USB connector is plugged into windows machine.")
+}
+
+type windowsAdapter struct {
+	windowsMachine *windows
+}
+
+func (w *windowsAdapter) insertIntoLightningPort() {
+	fmt.Println("Adapter converts Lightning signal to USB.")
+	w.windowsMachine.insertIntoUSBPort()
+}
+
+type client struct {
+}
+
+func (c *client) insertLightningConnectorIntoComputer(com computer) {
+	fmt.Println("Client inserts Lightning connector into computer.")
+	com.insertIntoLightningPort()
+}
+
+func main() {
+	client := client{}
+	mac := &mac{}
+	client.insertLightningConnectorIntoComputer(mac)
+
+	windows := &windows{}
+
+	windowsAdapter := &windowsAdapter{windowsMachine: windows}
+
+	client.insertLightningConnectorIntoComputer(windowsAdapter)
+}
