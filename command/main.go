7e1dac1ac3332f50ebf2f4816e59b238d95f890f
lamboktulus1379 <lamboktulus1379@gmail.com>
2022-07-25 15:18:42 +0700

    Added command

---
+++ command/main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+    tv := &Tv{}
+
+    onCommand := &OnCommand{
+        device: tv,
+    }
+
+    offCommand := &OffCommand{
+        device: tv,
+    }
+
+    onButton := &Button{
+        command: onCommand,
+    }
+    onButton.press()
+
+    offButton := &Button{
+        command: offCommand,
+    }
+    offButton.press()
+}
+
