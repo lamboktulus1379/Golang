cc1d8fcbf7dce314705827db561f35b54a5c92f5
lamboktulus1379 <lamboktulus1379@gmail.com>
2022-07-25 14:57:03 +0700

    Added chainofresponsibility

---
+++ chainofresponsibility/main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+
+	cashier := &Cashier{}
+
+	//Set next for medical department
+	medical := &Medical{}
+	medical.setNext(cashier)
+
+	//Set next for doctor department
+	doctor := &Doctor{}
+	doctor.setNext(medical)
+
+	//Set next for reception department
+	reception := &Reception{}
+	reception.setNext(doctor)
+
+	patient := &Patient{name: "abc"}
+	// patient2 := &Patient{name: "abc"}
+	//Patient visiting
+	reception.execute(patient)
+	reception.execute(patient)
+}
